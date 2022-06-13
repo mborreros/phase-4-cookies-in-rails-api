9932227f6873f3ee551b73b5dc1bea5a77d2df51
mborreros <mlbsindia@gmail.com>
2022-06-13 14:16:39 -0400

    commit tree 1bbedfe519cc74bc7c8b2d940ef48efa51074e5d

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
   
   def index
+    # byebug
     session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
+    cookies[:cookies_hello] ||= "TESTING"
     render json: { session: session, cookies: cookies.to_hash }
   end
 
